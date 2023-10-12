d0ce975dd714eaa634ff6299dd8716922d23a6e2
priya-singh-14 <singh.priya3@northeastern.edu>
2023-10-11 20:58:15 -0400

    updated accessor class

---
+++ db_accessor/user.accessor.js
@@ -0,0 +1,18 @@
+import Connection from "../db/connection.js";
+import User from "../models/user.js";
+
+export default class UserAccessor {
+    static async getAllUsers() {
+        try {
+            await Connection.open("users");
+            const users = [];
+            await Connection.open("users");
+            for await (const doc of User.find()) {
+                users.push(doc);
+            }
+            return users;
+        } catch (e) {
+            throw e;
+        }
+    }
+}
