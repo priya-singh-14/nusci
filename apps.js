cc99bffd7618a458f90e33fc4956e00cd796cac7
priya-singh-14 <singh.priya3@northeastern.edu>
2023-12-28 20:11:07 -0500

    syntax edits
    still need to fix follow-feature

--- apps.js
+++ apps.js
@@ -1,7 +1,7 @@
-import express from 'express'
-import cors from 'cors'
-import helmet from 'helmet'
-import route from './routes/pages.route.js'
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import route from "./routes/pages.route.js";
 
 const app = express();
 
@@ -11,7 +11,6 @@ app.use(cors());
 app.use(helmet());
 app.use(express.json());
 
-app.use("/", route)
-app.use("/page2", route)
+app.use("/", route);
 
-export default app
+export default app;
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -3,7 +3,7 @@ import UserAccessor from "../db_accessor/user.accessor.js";
 export default class UserController {
     static async getAllUsers (req, res) {
         const users = await UserAccessor.getAllUsers();
-        console.log (users);
+        console.log(users);
         res.render("index", { users: users } );
     }
 }
