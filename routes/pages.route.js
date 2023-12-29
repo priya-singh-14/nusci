300717cfa4bfa6b6262c44015be199be6127cb9e
priya-singh-14 <singh.priya3@northeastern.edu>
2023-12-28 19:06:52 -0500

    ?
    css isn't working, edits made

--- routes/pages.route.js
+++ routes/pages.route.js
@@ -1,5 +1,5 @@
-import express from 'express'
-import path from 'path'
+import express from "express";
+import path from "path";
 import UserController from "../controllers/user.controller.js";
 
 const router = express.Router()
